879722953412b9f15e4ba10b84cc8c55663b81ac
webandme1 <94754776+webandme1@users.noreply.github.com>
2024-01-12 10:16:27 -0700

    init

--- davis-react/src/components/Protfolio/SinglePortfolio.jsx
+++ davis-react/src/components/Protfolio/SinglePortfolio.jsx
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types';
 import { Icon } from '@iconify/react';
 
 const SinglePortfolio = ({ data, getData }) => {
-  const { imgLink, imgLinkLg, title, subTitle, effect, duration, delay } = data;
+  const { imgLink, imgLinkLg, title, subTitle, effect, duration, delay,targetUrl } = data;
 
   return (
     <div className="col-lg-4 col-md-6" data-aos={effect} data-aos-duration={duration} data-aos-delay={delay}>
-      <div className="st-portfolio-single st-style1" onClick={() => getData(imgLinkLg, title, subTitle)}>
+      <div className="st-portfolio-single st-style1" onClick={() => getData(imgLinkLg, title, subTitle,targetUrl)}>
         <div className="st-portfolio-item">
           <div className="st-portfolio st-zoom">
             <div className="st-portfolio-img st-zoom-in">
